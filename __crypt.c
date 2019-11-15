5043c7a0fdd8d73e36e4f792d6c8347a6e0f3ae0
GiandomenicoIameo <57752372+GiandomenicoIameo@users.noreply.github.com>
2019-11-15 10:50:02 +0100

    Update __crypt.c

--- __crypt.c
+++ __crypt.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 
-                            Crypto v1.0
+                            Crypto v0.0
                 Code, Compile, Run and Debug C program online.
 Write your code in this editor and press "Run" button to compile and execute it.
 
