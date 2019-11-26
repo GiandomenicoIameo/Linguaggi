b3d6bbfb101b8f20a56ffff3f30e1e9c5ab0c4ad
GiandomenicoIameo <giandomenicoiameo00@gmail.com>
2019-11-26 01:10:13 +0100

    truedecrypt

---
+++ Crypto/truedecrypt.c
@@ -0,0 +1,13 @@
+/* Decripta
+Si scriva un programma C che accetti un
+intero di quattro cifre crittografato con
+il metodo dell'esercizio precedente e
+restituisca il numero originale.
+
+Esempio di output:
+
+Inserisci un numero di quattro cifre: 4589
+
+Numero da decrittografare: 4589
+Numero originale: 1278
+*/
