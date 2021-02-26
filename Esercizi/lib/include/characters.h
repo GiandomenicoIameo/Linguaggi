737d7290a96b9be9799ab82787b2a9bac7ac7b0a
GiandomenicoIameo <g.iameo@studenti.unina.it>
2021-02-27 00:24:27 +0100

    Create characters.h

---
+++ Esercizi/lib/include/characters.h
@@ -0,0 +1,11 @@
+#ifndef CHARACTERS
+#define CHARACTERS
+
+#include <stdio.h>
+
+char isvowel( char c );
+unsigned int countdigit( char* str );
+unsigned int countcharacter( char* str );
+unsigned int countspace( char* str );
+
+#endif
