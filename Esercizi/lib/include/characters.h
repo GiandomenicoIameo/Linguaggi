1eebcd16113596f0bc1086aa0405164f03dcc8df
GiandomenicoIameo <g.iameo@studenti.unina.it>
2021-02-27 03:21:22 +0100

    Update

--- Esercizi/lib/include/characters.h
+++ Esercizi/lib/include/characters.h
@@ -4,8 +4,8 @@
 #include <stdio.h>
 
 char isvowel( char c );
-unsigned int countdigit( const char* str );
-unsigned int countcharacter( const char* str );
-unsigned int countspace( const char* str );
+unsigned int countdigit( const char *str );
+unsigned int countcharacter( const char *str );
+unsigned int countspace( const char *str );
 
 #endif
