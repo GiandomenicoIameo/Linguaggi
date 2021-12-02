0f568106dafcebc1500045f3933da427979d48c0
Giandomenico-bit <giandomenicoiameo00@gmail.com>
2021-12-02 16:38:35 +0100

    add

---
+++ Complessità computazionale/crivello.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int prime( int n );
+
+int main( void ) {
+
+  int n, res;
+
+  do {
+      printf( "Inserisci un numero : " );
+      scanf( "%d", &n );
+  } while( n < 0 );
+
+  res = prime( n );
+
+  if( res )
+    puts( "Il numero e' primo" );
+  else
+    puts( "Il numero non e' primo" );
+
+  return 0;
+}
+
+int prime( int n ) {
+
+  // La funzione verifica se n è un numero primo oppure il contrario.
+  // Complessità algoritmo O( n/6 ).
+
+  if( n == 2 ) {
+      return 1;
+  }
+  else if( n % 2 == 0 ) {
+      return 0;
+  }
+  if( n == 1 ) {
+      return 0;
+  }
+  int rest;
+  int div = 3;
+
+  while( div <= n / 3 ) {
+      rest = n % div;
+      if( !rest ) {
+          return 0;
+      }
+      div = div + 2;
+  }
+  return 1;
+}
