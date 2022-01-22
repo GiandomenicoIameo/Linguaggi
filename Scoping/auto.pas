0e2768f70d42d671582fd704ae4dd4ddef916681
GiandomenicoIameo <g.iameo@studenti.unina.it>
2022-01-22 04:27:01 +0100

    add

---
+++ Scoping/auto.pas
@@ -0,0 +1,18 @@
+program auto;
+
+  procedure s;
+    var a : integer;
+  begin
+    a := 10;
+  end;
+
+  procedure q;
+    var b : integer;
+  begin
+    writeln( b );
+  end;
+
+begin
+  s;
+  q;
+end.
