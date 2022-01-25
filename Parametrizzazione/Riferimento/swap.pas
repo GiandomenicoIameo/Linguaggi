7e4b4a8d4381283a24215283fc7afa77015dc5fd
GiandomenicoIameo <g.iameo@studenti.unina.it>
2022-01-25 18:47:52 +0100

    add

---
+++ Parametrizzazione/Riferimento/swap.pas
@@ -0,0 +1,22 @@
+program swap;
+  var a, b : integer;
+  procedure swap( var a, b : integer );
+    var tmp : integer;
+  begin
+    tmp := a;
+    a   := b;
+    b   := tmp;
+  end;
+begin
+  write( 'a : ' );
+  readln( a );
+
+  write( 'b : ' );
+  readln( b );
+
+  swap( a, b );
+
+  writeln( '' );
+  writeln( 'a : ', a );
+  writeln( 'b : ', b );
+end.
