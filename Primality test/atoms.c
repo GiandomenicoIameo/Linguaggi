e1e3ace4d3a1b5ef6ac3a23a7ac371abcb417642
GiandomenicoIameo <g.iameo@studenti.unina.it>
2022-04-13 18:52:11 +0200

    add

--- Primality test/atoms.c
+++ Primality test/atoms.c
@@ -4,47 +4,47 @@ int atomics( int n );
 
 int main( void ) {
 
-   int n;
-   do {
-           printf( "Inserisci un numero : " );
-           scanf( "%d", &n );
-   } while( n < 0 );
+    int n;
+    do {
+            printf( "Inserisci un numero : " );
+            scanf( "%d", &n );
+    } while( n < 0 );
 
-   if( atomics( n ) )
-           puts( "Il numero e' primo" );
-   else
-           puts( "Il numero non e' primo" );
+    if( atomics( n ) )
+            puts( "Il numero e' primo" );
+    else
+            puts( "Il numero non e' primo" );
 
-   return 0;
+    return 0;
 }
 
 int atomics( int n ) {
 
-  // La funzione verifica se n è un numero primo
-  // appartenente all'insieme dei numeri naturali.
-  //
-  // La complessità dell'algoritmo in termini di tempo e' : O( n/6 ).
-  // La complessità dell'algoritmo in termini di spazio e' : Θ( 1 ).
+    // La funzione verifica se n è un numero primo
+    // appartenente all'insieme dei numeri naturali.
+    //
+    // La complessità dell'algoritmo in termini di tempo e' : O( n/6 ).
+    // La complessità dell'algoritmo in termini di spazio e' : Θ( 1 ).
 
-   if( n == 2 )
-           return 1;
+    if( n == 2 )
+            return 1;
 
-   if( n % 2 == 0 )
-           return 0;
+    if( n % 2 == 0 )
+            return 0;
 
-   if( n == 1 )
-           return 0;
+    if( n == 1 )
+            return 0;
 
-   int rest;
-   int div = 3;
+    int rest;
+    int div = 3;
 
-   while( div <= n / 3 ) {
-       rest = n % div;
+    while( div <= n / 3 ) {
+        rest = n % div;
 
-       if( !rest )
-               return 0;
+        if( !rest )
+                return 0;
 
-       div = div + 2;
-   }
-   return 1;
+        div = div + 2;
+    }
+    return 1;
 }
