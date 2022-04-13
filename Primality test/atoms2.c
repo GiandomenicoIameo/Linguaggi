a034be1bce8c6454004945a24784d59381d87ee2
Giandomenico Iameo <57752372+GiandomenicoIameo@users.noreply.github.com>
2022-04-13 19:05:32 +0200

    Update atoms2.c

--- Primality test/atoms2.c
+++ Primality test/atoms2.c
@@ -30,51 +30,54 @@ bool positive( int n ) {
 
 void choice_number( int *pointer ) {
 
-     printf( "Inserisci un numero : " );
-     scanf( "%d", pointer );
+    printf( "Inserisci un numero : " );
+    scanf( "%d", pointer );
 
-     switch( positive( *pointer ) ) {
-     case true:
-             break;
-     case false:
-             choice_number( pointer );
-             break;
-     }
+    switch( positive( *pointer ) ) {
+    case true:
+            break;
+    case false:
+            choice_number( pointer );
+            break;
+    }
 }
 
 bool primes( int n ) {
 
-     int rest, div;
-     double square;
+    int rest, div;
+    double square;
 
-     // La funzione verifica se n è un numero primo
-     // appartenente all'insieme dei numeri naturali.
-     //
-     // La complessità dell'algoritmo in termini di tempo e' : O( 1/2 * sqrt( n ) ).
-     // La complessità dell'algoritmo in termini di spazio e' : Θ( 1 ).
+    // La funzione verifica se n è un numero primo
+    // appartenente all'insieme dei numeri naturali.
+    //
+    // La complessità dell'algoritmo in termini di tempo e' : O( 1/2 * sqrt( n ) ).
+    // La complessità dell'algoritmo in termini di spazio e' : Θ( 1 ).
 
-     div = 3;
-     square = sqrt( n );
+    div = 3;
+    square = sqrt( n );
 
-     while( div <= square ) {
-             rest = n % div;
+    while( div <= square ) {
+            rest = n % div;
 
-             if( !rest )
-                     return false;
+            if( !rest )
+                    return false;
 
-             div = div + 2;
-      }
-      return true;
+            div = div + 2;
+     }
+     return true;
 }
 
 bool atomics( int n ) {
 
-     if( n == 2 )
-             return true;
-     else if( n % 2 == 0 )
-             return false;
-     else if( n == 1 )
-             return false;
-     else
-             return primes( n );
+    if( n == 2 )
+            return true;
+    
+    else if( n % 2 == 0 )
+            return false;
+    
+    else if( n == 1 )
+            return false;
+    
+    else
+            return primes( n );
 }
